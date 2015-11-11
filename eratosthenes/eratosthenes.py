0838373ac4395efbcb43452b1c1b75e61c5dddc6
Ismail Negm <ismailnegm@gmail.com>
2015-11-10 21:23:16 -0500

    Adds Eratosthenes

---
+++ eratosthenes/eratosthenes.py
@@ -0,0 +1,21 @@
+# Eratosthene's sieve
+def multiples(list, div):
+    for n in list:
+        if n % div == 0:
+            list.remove(n)
+    return list
+
+
+def apply_sieve(end=120):
+    if end > 120:
+        raise ValueError('Your end number is too big! Come again.')
+    all_numbers = range(2, end)
+    first = [2, 3, 5, 7]
+    for f in first:
+        all_numbers = multiples(all_numbers, f)
+    primes = first + all_numbers
+    return primes
+
+
+if __name__ == "__main__":
+    print apply_sieve()
